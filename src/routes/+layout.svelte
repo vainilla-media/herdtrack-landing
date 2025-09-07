f6076d93cd27de1b03259e7aad2e65e55fe9b61f
Juan Saez <saez.juan08@gmail.com>
2025-09-07 17:08:10 -0300

    update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import '../app.css';
-import favicon from '$lib/assets/favicon.svg';
+import favicon from '$lib/assets/favicon.png';
 import SiteHeader from '$lib/components/SiteHeader.svelte';
 import SiteFooter from '$lib/components/SiteFooter.svelte';
 
